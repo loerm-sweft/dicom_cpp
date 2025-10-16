977f54fe299445a885cdd035bf843dbbea12533b
adfelh <142008759+adfelh@users.noreply.github.com>
2025-10-16 20:52:40 +0200

    Add vector and Windows headers to PrintSCP.h

--- src/PrintSCP.h
+++ src/PrintSCP.h
@@ -5,6 +5,7 @@
 #include <map>
 #include <mutex>
 #include <thread>
+#include <vector>
 
 // DCMTK Headers
 #include <dcmtk/config/osconfig.h>
@@ -14,6 +15,12 @@
 #include <dcmtk/dcmdata/dcdatset.h>
 #include <dcmtk/dcmdata/dcdeftag.h>
 #include <dcmtk/dcmdata/dctk.h>
+#include <dcmtk/dcmimgle/dcmimage.h> // لإدارة الصور الطبية
+
+// Windows Headers للطباعة
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 class PrintSCP {
 private:
@@ -25,27 +32,34 @@ public:
     PrintSCP();
     virtual ~PrintSCP();
 
-    // معالجة الاتصال المبسطة
+    // معالجة الاتصال
     OFCondition handleAssociation(T_ASC_Association* assoc);
 
 protected:
-    // معالجة طلبات DIMSE المبسطة
+    // معالجة طلبات DIMSE
     virtual OFCondition handleNCreateRequest(const T_DIMSE_N_CreateRQ& req,
-                                           T_ASC_PresentationContextID presID);
+                                             T_ASC_PresentationContextID presID);
     
     virtual OFCondition handleNActionRequest(const T_DIMSE_N_ActionRQ& req,
-                                           T_ASC_PresentationContextID presID);
+                                             T_ASC_PresentationContextID presID);
     
     virtual OFCondition handleNDeleteRequest(const T_DIMSE_N_DeleteRQ& req,
-                                           T_ASC_PresentationContextID presID);
+                                             T_ASC_PresentationContextID presID);
 
 private:
+    // إنشاء جلسة/صندوق فيلم وطابعة
     OFCondition handleFilmSessionCreate();
     OFCondition handleFilmBoxCreate();
     OFCondition handlePrinterCreate();
     
-    // إرسال ردود DIMSE
+    // إرسال ردود N-CREATE
     OFCondition sendNCreateResponse(const T_DIMSE_N_CreateRQ& req,
-                                   T_ASC_PresentationContextID presID,
-                                   Uint16 status);
+                                    T_ASC_PresentationContextID presID,
+                                    Uint16 status);
+
+    // دالة مساعدة للطباعة على الطابعة
+    bool sendToPrinter(const Uint8* buffer,
+                       unsigned long width,
+                       unsigned long height,
+                       const std::string& printerName = "");
 };
